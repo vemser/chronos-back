c1840daf65a21caae2bafd9a4972db305a4bdcc0
Luiz Martins <luiiz.martiins@outlook.com>
2022-11-25 18:20:02 -0300

    criação do projeto spring chronos-api

---
+++ src/test/java/br/com/dbc/chronosapi/ChronosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.chronosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
