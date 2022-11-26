74e43e72d5562ebca8451e3161c4b192648b4c65
uLinck <linck.privado@gmail.com>
2022-11-26 00:06:05 -0300

    add: novas tabelas e sequences no sql do bd

--- src/main/java/br/com/dbc/chronosapi/entity/EdicaoEntity.java
+++ src/main/java/br/com/dbc/chronosapi/entity/EdicaoEntity.java
@@ -1,4 +1,12 @@
 package br.com.dbc.chronosapi.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity("EDICAO")
 public class EdicaoEntity {
 }
