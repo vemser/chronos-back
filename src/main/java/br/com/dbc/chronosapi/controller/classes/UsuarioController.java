3bd1e69d44f65da3ca6d554b00e6c28c3d256114
Luiz Martins <luiiz.martiins@outlook.com>
2022-11-27 10:37:04 -0300

    add: implementei a documentação da classe UsuarioController por meio de uma interface IUsuarioController seguindo o padrão das demais documentações criadas no projeto.

--- src/main/java/br/com/dbc/chronosapi/controller/classes/UsuarioController.java
+++ src/main/java/br/com/dbc/chronosapi/controller/classes/UsuarioController.java
@@ -1,58 +1,64 @@
 package br.com.dbc.chronosapi.controller.classes;
 
+import br.com.dbc.chronosapi.controller.interfaces.IUsuarioController;
 import br.com.dbc.chronosapi.dto.PageDTO;
+import br.com.dbc.chronosapi.dto.UsuarioCreateDTO;
 import br.com.dbc.chronosapi.dto.UsuarioDTO;
+import br.com.dbc.chronosapi.dto.UsuarioUpdateDTO;
 import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.chronosapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
 
 @Slf4j
 @Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/usuario")
-public class UsuarioController {
+public class UsuarioController implements IUsuarioController {
 
     private final UsuarioService usuarioService;
 
     @GetMapping
-    public ResponseEntity<PageDTO<UsuarioDTO>> list(Integer pagina, Integer tamanho) throws RegraDeNegocioException {
-        return ResponseEntity.ok(usuarioService.list(pagina, tamanho));
+    public ResponseEntity<PageDTO<UsuarioDTO>> list(Integer pagina, Integer tamanho) {
+        return new ResponseEntity<>(usuarioService.list(pagina, tamanho), HttpStatus.OK);
     }
 
-//    @PostMapping("/registration")
-//    public ResponseEntity<UsuarioDTO> registration(@Valid @RequestBody UsuarioCreateDTO usuario) throws RegraDeNegocioException {
-//
-//        log.info("Cadastro de usuário em andamento . . .");
-//        UsuarioDTO usuarioDTO = usuarioService.registerUser(usuario);
-//        log.info("Cadastro realizado.");
-//
-//        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
-//    }
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<UsuarioDTO> create(@Valid @RequestBody UsuarioCreateDTO usuario,
+                                             @RequestParam MultipartFile imagem) throws RegraDeNegocioException, IOException {
+        log.info("Cadastrando usuário...");
+        UsuarioDTO usuarioDTO = usuarioService.create(usuario, imagem);
+        log.info("Usuário cadastrado com sucesso!");
 
-    //    @PutMapping("/edit-register")
-//    public ResponseEntity<UsuarioDTO> update(@Valid @RequestBody UsuarioCreateDTO usuarioCreateDTO) throws RegraDeNegocioException {
-//        log.info("Atualizando usuário....");
-//        UsuarioDTO usuarioDTO = usuarioService.edit(usuarioCreateDTO);
-//        log.info("Usuário atualizado com sucesso!");
-//
-//        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
-//    }
+        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
+    }
 
-//    @DeleteMapping("/{idUsuario}") // localhost:1521/pessoa/10
-//    public ResponseEntity<UsuarioDTO> delete(@PathVariable("idUsuario") Integer id) throws RegraDeNegocioException {
-//
-//        log.info("Deletando a pessoa");
-//        usuarioService.remover(id);
-//        log.info("Deletado com sucesso!");
-//
-//        return ResponseEntity.noContent().build();
-//    }
+        @PutMapping("/{idUsuario}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable("idUsuario") Integer idUsuario,
+                                             @Valid @RequestBody UsuarioUpdateDTO usuarioUpdateDTO) throws RegraDeNegocioException {
+        log.info("Atualizando usuário....");
+        UsuarioDTO usuarioDTO = usuarioService.update(idUsuario, usuarioUpdateDTO);
+        log.info("Usuário atualizado com sucesso!");
 
+        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity<UsuarioDTO> delete(@PathVariable("idUsuario") Integer idUsuario) throws RegraDeNegocioException {
+        log.info("Deletando usuário...");
+        usuarioService.delete(idUsuario);
+        log.info("Usuário deletado com sucesso!");
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/dbc/chronosapi/service/UsuarioService.java
+++ src/main/java/br/com/dbc/chronosapi/service/UsuarioService.java
@@ -1,11 +1,14 @@
 package br.com.dbc.chronosapi.service;
 
-import br.com.dbc.chronosapi.dto.*;
+import br.com.dbc.chronosapi.dto.PageDTO;
+import br.com.dbc.chronosapi.dto.UsuarioCreateDTO;
+import br.com.dbc.chronosapi.dto.UsuarioDTO;
+import br.com.dbc.chronosapi.dto.UsuarioUpdateDTO;
 import br.com.dbc.chronosapi.entity.classes.CargoEntity;
 import br.com.dbc.chronosapi.entity.classes.UsuarioEntity;
+import br.com.dbc.chronosapi.entity.enums.StatusUsuario;
 import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.chronosapi.repository.UsuarioRepository;
-import br.com.dbc.chronosapi.security.TokenService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,11 +31,22 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final ObjectMapper objectMapper;
-    private final TokenService tokenService;
-    private final EmailService emailService;
     private final CargoService cargoService;
     private final PasswordEncoder passwordEncoder;
 
+    public PageDTO<UsuarioDTO> list(Integer pagina, Integer tamanho) {
+        PageRequest pageRequest = PageRequest.of(pagina, tamanho);
+        Page<UsuarioEntity> paginaDoRepositorio = usuarioRepository.findAll(pageRequest);
+        List<UsuarioDTO> usuarios = paginaDoRepositorio.getContent().stream()
+                .map(usuario -> objectMapper.convertValue(usuario, UsuarioDTO.class))
+                .toList();
+        return new PageDTO<>(paginaDoRepositorio.getTotalElements(),
+                paginaDoRepositorio.getTotalPages(),
+                pagina,
+                tamanho,
+                usuarios
+        );
+    }
 
     public UsuarioDTO create(UsuarioCreateDTO usuario, MultipartFile imagem) throws IOException, RegraDeNegocioException {
         UsuarioEntity usuarioEntity = objectMapper.convertValue(usuario, UsuarioEntity.class);
@@ -41,6 +56,8 @@ public class UsuarioService {
         usuarioEntity.setImagem(imagem.getBytes());
         Set<CargoEntity> cargos = usuario.getCargos().stream()
                 .map(cargo -> cargoService.findByNome(cargo)).collect(Collectors.toSet());
+        usuarioEntity.setCargos(new HashSet<>(cargos));
+        usuarioEntity.setStatus(StatusUsuario.ATIVO);
         return objectMapper.convertValue(usuarioRepository.save(usuarioEntity), UsuarioDTO.class);
     }
 
@@ -60,22 +77,12 @@ public class UsuarioService {
         return null;
     }
 
-    public PageDTO<UsuarioDTO> list(Integer pagina, Integer tamanho) {
-        PageRequest pageRequest = PageRequest.of(pagina, tamanho);
-        Page<UsuarioEntity> paginaDoRepositorio = usuarioRepository.findAll(pageRequest);
-        List<UsuarioDTO> usuarios = paginaDoRepositorio.getContent().stream()
-                .map(usuario -> objectMapper.convertValue(usuario, UsuarioDTO.class))
-                .toList();
-        return new PageDTO<>(paginaDoRepositorio.getTotalElements(),
-                paginaDoRepositorio.getTotalPages(),
-                pagina,
-                tamanho,
-                usuarios
-        );
+    public void delete(Integer idUsuario) throws RegraDeNegocioException {
+        UsuarioEntity usuarioEntity = this.findById(idUsuario);
+        usuarioRepository.delete(usuarioEntity);
     }
 
-    public UsuarioEntity
-    findByEmail(String email) {
+    public UsuarioEntity findByEmail(String email) {
         return usuarioRepository.findByEmail(email);
     }
 
