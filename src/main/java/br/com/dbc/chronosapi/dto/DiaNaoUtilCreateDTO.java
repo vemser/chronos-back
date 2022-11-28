fb4990b9c242fc3bca4217543f105a4ea211ae6d
uLinck <linck.privado@gmail.com>
2022-11-28 20:58:04 -0300

    update: implementando atributos e validacoes

--- src/main/java/br/com/dbc/chronosapi/dto/DiaNaoUtilCreateDTO.java
+++ src/main/java/br/com/dbc/chronosapi/dto/DiaNaoUtilCreateDTO.java
@@ -1,20 +1,30 @@
 package br.com.dbc.chronosapi.dto;
 
 import br.com.dbc.chronosapi.entity.enums.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 public class DiaNaoUtilCreateDTO {
 
+    @NotNull
+    @NotBlank
+    @Schema(description = "descricao do dia não util")
     private String descricao;
 
+    @NotNull
+    @Schema(description = "data inicial do dia não util")
     private LocalDate dataInicial;
 
+    @Schema(description = "data final do dia não util")
     private LocalDate dataFinal;
 
+    @NotNull
+    @Schema(description = "status da repetição anual do dia não util")
     private Status repeticaoAnual;
 
 }
--- src/main/java/br/com/dbc/chronosapi/dto/DiaNaoUtilDTO.java
+++ src/main/java/br/com/dbc/chronosapi/dto/DiaNaoUtilDTO.java
@@ -1,4 +1,6 @@
 package br.com.dbc.chronosapi.dto;
 
 public class DiaNaoUtilDTO {
+
+    private Integer idDiaNaoUtil;
 }
