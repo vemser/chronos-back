b032687b39c95bd26975767794a95a3c808ef5f2
uLinck <linck.privado@gmail.com>
2022-11-28 20:54:38 -0300

    update: criando service e implementando list/findById

---
+++ src/main/java/br/com/dbc/chronosapi/service/DiaNaoUtilService.java
@@ -0,0 +1,62 @@
+package br.com.dbc.chronosapi.service;
+
+import br.com.dbc.chronosapi.dto.DiaNaoUtilCreateDTO;
+import br.com.dbc.chronosapi.dto.DiaNaoUtilDTO;
+import br.com.dbc.chronosapi.dto.PageDTO;
+import br.com.dbc.chronosapi.dto.edicao.EdicaoDTO;
+import br.com.dbc.chronosapi.entity.classes.DiaNaoUtilEntity;
+import br.com.dbc.chronosapi.entity.classes.EdicaoEntity;
+import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
+import br.com.dbc.chronosapi.repository.DiaNaoUtilRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class DiaNaoUtilService {
+
+    private final ObjectMapper objectMapper;
+    private final DiaNaoUtilRepository diaNaoUtilRepository;
+
+
+    public DiaNaoUtilDTO create(DiaNaoUtilCreateDTO diaNaoUtilCreateDTO) {
+
+        // code
+
+    }
+
+    public DiaNaoUtilDTO update(Integer idDiaNaoUtil, DiaNaoUtilCreateDTO diaNaoUtilUpdate) {
+
+        // code
+
+    }
+
+    public void delete(Integer idDiaNaoUtil) {
+
+    }
+
+    public PageDTO<DiaNaoUtilDTO> list(Integer pagina, Integer tamanho) {
+        PageRequest pageRequest = PageRequest.of(pagina, tamanho);
+        Page<DiaNaoUtilEntity> paginaDoRepositorio = diaNaoUtilRepository.findAll(pageRequest);
+        List<DiaNaoUtilDTO> diaNaoUtilDTOList = paginaDoRepositorio.getContent().stream()
+                .map(diaNaoUtil -> objectMapper.convertValue(diaNaoUtil, DiaNaoUtilDTO.class))
+                .toList();
+        return new PageDTO<>(paginaDoRepositorio.getTotalElements(),
+                paginaDoRepositorio.getTotalPages(),
+                pagina,
+                tamanho,
+                diaNaoUtilDTOList);
+    }
+
+    public DiaNaoUtilEntity findById(Integer id) throws RegraDeNegocioException {
+        DiaNaoUtilEntity diaNaoUtilEntity = diaNaoUtilRepository.findById(id)
+                .orElseThrow(() -> new RegraDeNegocioException("Dia não util não encontrado"));
+        return diaNaoUtilEntity;
+    }
+
+}
