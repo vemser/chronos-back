4a071b415fcd81ce0d4872b83c5c60b756368ef9
uLinck <linck.privado@gmail.com>
2022-11-29 11:39:18 -0300

    fix: reformulando metodos create e update

--- src/main/java/br/com/dbc/chronosapi/service/ProcessoService.java
+++ src/main/java/br/com/dbc/chronosapi/service/ProcessoService.java
@@ -1,10 +1,14 @@
 package br.com.dbc.chronosapi.service;
 
 import br.com.dbc.chronosapi.dto.PageDTO;
+import br.com.dbc.chronosapi.dto.processo.AreaEnvolvidaDTO;
 import br.com.dbc.chronosapi.dto.processo.ProcessoCreateDTO;
 import br.com.dbc.chronosapi.dto.processo.ProcessoDTO;
+import br.com.dbc.chronosapi.dto.processo.ResponsavelDTO;
 import br.com.dbc.chronosapi.entity.classes.EtapaEntity;
+import br.com.dbc.chronosapi.entity.classes.processos.AreaEnvolvidaEntity;
 import br.com.dbc.chronosapi.entity.classes.processos.ProcessoEntity;
+import br.com.dbc.chronosapi.entity.classes.processos.ResponsavelEntity;
 import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.chronosapi.repository.ProcessoRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,13 +17,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProcessoService {
     private final ProcessoRepository processoRepository;
     private final ObjectMapper objectMapper;
+    private final ResponsavelService responsavelService;
+    private final AreaEnvolvidaService areaEnvolvidaService;
 
     private final EtapaService etapaService;
 
@@ -40,22 +49,27 @@ public class ProcessoService {
         EtapaEntity etapaEntity = etapaService.findById(idEtapa);
         ProcessoEntity processoEntity = objectMapper.convertValue(processoCreateDTO, ProcessoEntity.class);
         processoEntity.setEtapa(etapaEntity);
-        return objectMapper.convertValue(processoRepository.save(processoEntity), ProcessoDTO.class);
+        ProcessoEntity processoSaved = processoRepository.save(processoEntity);
+        etapaEntity.getProcessos().add(processoSaved);
+        etapaService.save(etapaEntity);
+        return objectMapper.convertValue(processoEntity, ProcessoDTO.class);
     }
 
     public ProcessoDTO update(Integer idProcesso, ProcessoCreateDTO processoUpdate) throws RegraDeNegocioException {
-//        ProcessoDTO processoRecoverDTO = objectMapper.convertValue(this.findById(idProcesso), ProcessoDTO.class);
-//        processoRecoverDTO.setNome(processoUpdate.getNome());
-//        processoRecoverDTO.setEtapa(processoUpdate.getEtapa());
-//        processoRecoverDTO.setAreaEnvolvida(processoUpdate.getAreaEnvolvida());
-//        processoRecoverDTO.setResponsavel(processoRecoverDTO.getResponsavel());
-//        processoRecoverDTO.setDiasUteis(processoUpdate.getDiasUteis());
-//        processoRecoverDTO.setOrdemExecucao(processoUpdate.getOrdemExecucao());
-//        processoRecoverDTO.setDuracaoProcesso(processoUpdate.getDuracaoProcesso());
-//        processoRecoverDTO.setEdicoes(processoUpdate.getEdicoes());
-//        ProcessoEntity processoEntity = objectMapper.convertValue(processoRecoverDTO, ProcessoEntity.class);
-//        return objectMapper.convertValue(processoRepository.save(processoEntity), ProcessoDTO.class);
-        return null;
+        ProcessoEntity processoRecover = findById(idProcesso);
+        processoRecover.setOrdemExecucao(processoUpdate.getOrdemExecucao());
+        processoRecover.setDuracaoProcesso(processoUpdate.getDuracaoProcesso());
+        processoRecover.setDiasUteis(processoUpdate.getDiasUteis());
+        Set<ResponsavelEntity> responsaveisEntities = processoUpdate.getResponsavel().stream()
+                        .map(responsavel -> (responsavelService.findByNome(responsavel))).collect(Collectors.toSet());
+        processoRecover.setResponsaveis(new HashSet<>(responsaveisEntities));
+        Set<AreaEnvolvidaEntity> areaEnvolvidasEntities = processoUpdate.getAreaEnvolvida().stream()
+                .map(areasEnvolvidas -> (areaEnvolvidaService.findByNome(areasEnvolvidas))).collect(Collectors.toSet());
+        processoRecover.setAreasEnvolvidas(new HashSet<>(areaEnvolvidasEntities));
+        ProcessoDTO processoDTO = objectMapper.convertValue(processoRepository.save(processoRecover), ProcessoDTO.class);
+        processoDTO.setResponsavel(getResponsavelDTO(responsaveisEntities));
+        processoDTO.setAreaEnvolvida(getAreaEnvolvidaDTO(areaEnvolvidasEntities));
+        return processoDTO;
     }
 
     public void delete(Integer isProcesso) throws RegraDeNegocioException {
@@ -67,4 +81,17 @@ public class ProcessoService {
         return processoRepository.findById(idProcesso)
                 .orElseThrow(() -> new RegraDeNegocioException("Processo não encontrado!"));
     }
+
+    public Set<ResponsavelDTO> getResponsavelDTO(Set<ResponsavelEntity> responsaveis) {
+        return responsaveis.stream()
+                .map(responsavelEntity -> objectMapper.convertValue(responsavelEntity, ResponsavelDTO.class))
+                .collect(Collectors.toSet());
+    }
+    public Set<AreaEnvolvidaDTO> getAreaEnvolvidaDTO(Set<AreaEnvolvidaEntity> AreasEnvolvidas) {
+        return AreasEnvolvidas.stream()
+                .map(areaEnvolvidaEntity -> objectMapper.convertValue(areaEnvolvidaEntity, AreaEnvolvidaDTO.class))
+                .collect(Collectors.toSet());
+    }
+
 }
+
