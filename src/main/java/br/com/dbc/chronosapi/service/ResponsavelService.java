791a70f6b2a332190aca643bd16a07337b8c892c
uLinck <linck.privado@gmail.com>
2022-11-29 13:02:18 -0300

    update: criando metodos de create e delete

--- src/main/java/br/com/dbc/chronosapi/service/ResponsavelService.java
+++ src/main/java/br/com/dbc/chronosapi/service/ResponsavelService.java
@@ -1,8 +1,11 @@
 package br.com.dbc.chronosapi.service;
 
+import br.com.dbc.chronosapi.dto.processo.ResponsavelCreateDTO;
+import br.com.dbc.chronosapi.dto.processo.ResponsavelDTO;
 import br.com.dbc.chronosapi.entity.classes.processos.ResponsavelEntity;
 import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.chronosapi.repository.ResponsavelRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ResponsavelService {
     private final ResponsavelRepository responsavelRepository;
+    private final ObjectMapper objectMapper;
+
+    public ResponsavelDTO create(ResponsavelCreateDTO responsavelCreateDTO) {
+        ResponsavelEntity responsavelEntity = objectMapper.convertValue(responsavelCreateDTO, ResponsavelEntity.class);
+        responsavelEntity.setNome(responsavelCreateDTO.getNome());
+        return objectMapper.convertValue(responsavelRepository.save(responsavelEntity), ResponsavelDTO.class);
+    }
+
+    public void delete(Integer idResponsavel) throws RegraDeNegocioException {
+        ResponsavelEntity responsavelRecover = findById(idResponsavel);
+        responsavelRepository.delete(responsavelRecover);
+    }
 
     public ResponsavelEntity findById(Integer id) throws RegraDeNegocioException {
         return responsavelRepository.findById(id)
