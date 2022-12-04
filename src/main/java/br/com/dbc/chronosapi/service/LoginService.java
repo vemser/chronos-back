e32a8c3c79cf83c4ffd6b1328e677fd5e06df0aa
uLinck <linck.privado@gmail.com>
2022-12-04 15:00:23 -0300

    fix: corrigindo metodo do envio de recuperacao de senha

--- src/main/java/br/com/dbc/chronosapi/service/LoginService.java
+++ src/main/java/br/com/dbc/chronosapi/service/LoginService.java
@@ -4,6 +4,7 @@ import br.com.dbc.chronosapi.dto.usuario.UsuarioDTO;
 import br.com.dbc.chronosapi.entity.classes.UsuarioEntity;
 import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.chronosapi.repository.UsuarioRepository;
+import br.com.dbc.chronosapi.security.TokenService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,6 +21,8 @@ public class LoginService {
     private final UsuarioRepository usuarioRepository;
     private final EmailService emailService;
     private final PasswordEncoder passwordEncoder;
+
+    private final TokenService tokenService;
 
     public Integer getIdLoggedUser() {
         return Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
@@ -41,14 +44,15 @@ public class LoginService {
         throw new RegraDeNegocioException("Usuario não encontrado!");
     }
 
-//    public String sendRecoverPasswordEmail(String email) {
-//        UsuarioDTO usuario = objectMapper.convertValue(usuarioRepository.findByEmail(email), UsuarioDTO.class);
+    public String sendRecoverPasswordEmail(String email) {
+        UsuarioEntity usuarioEntity = usuarioRepository.findByEmail(email);
+        UsuarioDTO usuario = objectMapper.convertValue(usuarioEntity, UsuarioDTO.class);
 
-//        String token = tokenService.getToken(usuario, true);
-//        emailService.sendEmailRecuperacaoSenha(usuario, usuario.getSenha());
-//
-//        return "Verifique seu email para trocar a senha.";
-//    }
+        String token = tokenService.getToken(usuarioEntity, true);
+        emailService.sendEmailRecuperacaoSenha(usuario, token);
+
+        return "Verifique seu email para trocar a senha.";
+    }
 
     public Optional<UsuarioEntity> findByEmailAndSenha(String login, String senha) {
         return usuarioRepository.findByEmailAndSenha(login, senha);
