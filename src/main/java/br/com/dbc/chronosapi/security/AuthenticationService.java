3d4dfe6ca6696964fff582fc354087102211cbd9
uLinck <linck.privado@gmail.com>
2022-12-08 17:26:39 -0300

    fix: tirando RunTimeException

--- src/main/java/br/com/dbc/chronosapi/security/AuthenticationService.java
+++ src/main/java/br/com/dbc/chronosapi/security/AuthenticationService.java
@@ -1,7 +1,6 @@
 package br.com.dbc.chronosapi.security;
 
 import br.com.dbc.chronosapi.entity.classes.UsuarioEntity;
-import br.com.dbc.chronosapi.exceptions.RegraDeNegocioException;
 import br.com.dbc.chronosapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,11 +16,7 @@ public class AuthenticationService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UsuarioEntity usuario = null;
-        try {
-            usuario = usuarioService.findByEmail(email);
-        } catch (RegraDeNegocioException e) {
-            throw new RuntimeException(e);
-        }
+        usuario = usuarioService.findByEmail(email);
         if(usuario == null) {
             throw new UsernameNotFoundException("Usuario inválido");
         } else {
