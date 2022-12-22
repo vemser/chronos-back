f087d088814d5653383d3d950f211cabf7abd2ff
Luiz Martins <luiiz.martiins@outlook.com>
2022-12-22 14:25:24 -0300

    fix: corrigido bug de geração de calendario

--- src/main/java/br/com/dbc/chronosapi/service/EdicaoService.java
+++ src/main/java/br/com/dbc/chronosapi/service/EdicaoService.java
@@ -224,7 +224,7 @@ public class EdicaoService {
                                 }
                             }
                             return processoEntity;
-                        }).toList());
+                        }).toList()).toList();
         return dias;
     }
 
